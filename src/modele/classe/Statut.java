082b2a2f4797c1580e61ed2f39ecf7dfe8bbb743
weiss54 <72012859+weiss54@users.noreply.github.com>
2021-12-17 16:53:38 +0100

    Ajout de l'enumeration Statut et TypeClasse

---
+++ src/modele/classe/Statut.java
@@ -0,0 +1,11 @@
+// PACKAGE
+package modele.classe;
+
+/**
+ * Enumeration Statut, permet de definir le statut d'une classe
+ */
+public enum Statut {
+    PUBLIC,
+    PRIVATE,
+    PROTECTED
+}
---
+++ src/modele/classe/TypeClasse.java
@@ -0,0 +1,12 @@
+// PACKAGE
+package modele.classe;
+
+/**
+ * Enumeration TypeClasse, permet de definir le type de classe qu'on cree
+ */
+public enum TypeClasse {
+    CLASSE,
+    INTERFACE,
+    ENUM,
+    ABSTRACT
+}
