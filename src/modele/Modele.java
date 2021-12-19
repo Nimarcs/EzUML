f466cff2e5fd2c6bd41169e16cbd6d99ccbd71a4
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2021-12-19 14:08:03 +0100

    Creation de Modele
    Création du squelette de modèle avec les classes présente sur le diagramme de classe
    Ajout des attributs et du constructeur

---
+++ src/modele/Modele.java
@@ -0,0 +1,150 @@
+package modele;
+
+import java.io.File;
+import java.util.HashSet;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Set;
+
+/**
+ * Modele permet de mettre en commun les differents elements du logiciel
+ * Contient ce qui est necessaire pour armoniser le diagramme, l'arborescence et les differents controleurs et vues
+ * @author Marcus RICHIER
+ */
+public class Modele extends Sujet {
+
+
+    //Attributs
+
+
+    /**
+     * Booleen a vrai si et seulement si la touche controle du clavier est en cours d'appui
+     * Permet d'effectuer des combinaisons de touche comme ctrl+clic pour ajouter des elements a la selection
+     */
+    private boolean ctrlActive;
+
+    /**
+     * Ensemble des classes chargee dans le programme
+     * Les classes elle meme contiennent l'information sachant si elles sont visibles
+     */
+    private Set<ObjectClasse> classes;
+
+    /**
+     * Booleen a vrai si on affiche les packages dans lesquels sont les classes sous forme textuelle, faux sinon
+     */
+    private boolean afficherPackage;
+
+    /**
+     * Liste des classes selectionne au moment courant
+     * On ne peut selectionner que des classes
+     */
+    private List<ObjectClasse> selection;
+
+    /**
+     * Fichier charge couramment, sert a l'arboresance
+     */
+    private File dossierProjet;
+
+    /**
+     * Facade qui permet d'obtenir les ObjectClasse a partir des fichiers
+     */
+    private FacadeIntrospection facade;
+
+
+    //Contructeurs
+
+
+    /**
+     * Contructeur de Modele
+     * initialise les attributs de modeles
+     */
+    public Modele(){
+        classes = new HashSet<>();
+        selection = new LinkedList<>();
+        afficherPackage = true;
+        ctrlActive = false;
+        dossierProjet = null; // pas de dossier chargee de base
+    }
+
+
+    //Methodes
+
+    /**
+     * methode qui permet de selectionner une classe
+     * Si la classe est deja selectionne, la deselectionne
+     * @param objectClasse ObjectClasse a selectionner ou deselectionner
+     */
+    public void selectionnerUneClasse(ObjectClasse objectClasse){
+    }
+
+    /**
+     * Methode qui vide la selection
+     */
+    public void deselectionner(){
+    }
+
+    /**
+     * Methode qui permet de deplacer la ou les classes selectionnee de x et y
+     * @param x deplacement sur l'axe des abscisses
+     * @param y deplacement sur l'axe des ordonnees
+     */
+    public void deplacerSelection(int x, int y){
+        throw new IllegalStateException("PAS FAIT");
+    }
+
+    /**
+     * methode qui permet d'alterner entre masquer ou afficher les packages sous forme textuelle
+     */
+    public void changerAffichagePackage(){
+    }
+
+    /**
+     * methode qui permet de reinitialiser le diagramme
+     */
+    public void reintialiserDiagramme(){
+    }
+
+    /**
+     * Methode qui permet de charger une classe en lui donnant un fichier
+     * @param f fichier a charger
+     */
+    private void chargerClasse(File f){
+    }
+
+    /**
+     * methode qui permet de charger l'arborescence et les different fichier la composant
+     * @param f fichier a tester
+     */
+    public void chargerArborescenceProjet(File f){
+        throw new IllegalStateException("PAS FAIT");
+        /*
+        notifierObservateurs();
+        dossierProjet = f;
+
+         */
+    }
+
+
+    //getters setters
+
+
+    public boolean isCtrlActive() {
+        return ctrlActive;
+    }
+
+    public Set<ObjectClasse> getClasses() {
+        return classes;
+    }
+
+    public File getDossierProjet() {
+        return dossierProjet;
+    }
+
+    public List<ObjectClasse> getSelection() {
+        return selection;
+    }
+
+    public void setCtrlActive(boolean ctrlActive) {
+        this.ctrlActive = ctrlActive;
+    }
+}
