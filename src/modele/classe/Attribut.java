b3525ff9b54eb0e1722d2164fabcfc9e31283cf1
weiss54 <72012859+weiss54@users.noreply.github.com>
2021-12-21 17:27:09 +0100

    Ajout des attributs de la classe Attribut

--- src/modele/classe/Attribut.java
+++ src/modele/classe/Attribut.java
@@ -11,6 +11,35 @@ public class Attribut {
 
     // ATTRIBUTS
 
+    /**
+     * Attribut prive String contenant le nom de l'attribut
+     */
+    private String nomAttribut;
+
+    /**
+     * Attribut prive Statut qui contient le statut de l'attribut
+     */
+    private Statut statutAttribut;
+
+    /**
+     * Attribut prive String contenant le type d'objet qu'est l'attribut
+     */
+    private String typeAttribut;
+
+    /**
+     * Attribut prive booleen qui permet de definir si l'attribut est statique ou non
+     */
+    private boolean statique;
+
+    /**
+     * Attribut prive booleen qui permet de definir si l'attribut est final ou non
+     */
+    private boolean finale;
+
+    /**
+     * Attribut prive boolean indiquant la visibilite de l'attribut dans la zone d'affichage du diagramme
+     */
+    private boolean visible;
 
     // CONSTRUCTEURS
 
