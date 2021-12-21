a624e1089e7515a2a52c5ee626fddab1a01f150f
weiss54 <72012859+weiss54@users.noreply.github.com>
2021-12-21 17:12:45 +0100

    Création de la classe Attribut

---
+++ src/modele/classe/Attribut.java
@@ -0,0 +1,23 @@
+// PACKAGE
+package modele.classe;
+
+/**
+ * Classe Attribut
+ *
+ * Représente un attribut au sein d'une classe
+ * Contient l'information concernant son statut, son type et son nom
+ */
+public class Attribut {
+
+    // ATTRIBUTS
+
+
+    // CONSTRUCTEURS
+
+
+    // METHODES
+
+
+    // GETTERS && SETTERS
+
+}
