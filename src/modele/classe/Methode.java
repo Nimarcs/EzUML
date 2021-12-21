15767631d6a6f28b96417c47254a760c74f0f5d1
weiss54 <72012859+weiss54@users.noreply.github.com>
2021-12-21 22:49:42 +0100

    Ajout des attributs de la classe Methode

--- src/modele/classe/Methode.java
+++ src/modele/classe/Methode.java
@@ -1,6 +1,9 @@
 // PACKAGE
 package modele.classe;
 
+// IMPORTS
+import java.util.List;
+
 /**
  * Classe Methode
  *
@@ -11,6 +14,45 @@ public class Methode {
 
     // ATTRIBUTS
 
+    /**
+     * Attribut prive String contenant le nom de la methode
+     */
+    private String nomMethode;
+
+    /**
+     * Attribut prive Statut definissant le statut de la methode
+     */
+    private Statut statutMethode;
+
+    /**
+     * Attribut prive String contenant le type de retour, null si le retour est void
+     */
+    private String typeRetour;
+
+    /**
+     * Attribut prive liste de String contenant la liste des types de parametres
+     */
+    private List<String> listeParametres;
+
+    /**
+     * Attribut prive booleen indiquant si la methode est abstraite ou non
+     */
+    private boolean abstrait;
+
+    /**
+     * Attribut prive booleen indiquant si la methode est statique ou non
+     */
+    private boolean statique;
+
+    /**
+     * Attribut prive booleen indiquant si la methode est finale ou non
+     */
+    private boolean finale;
+
+    /**
+     * Attribut prive booleen indiquant la visibilite de la methode dans la zone d'affichage du diagramme
+     */
+    private boolean visible;
 
     // CONSTRUCTEURS
 
