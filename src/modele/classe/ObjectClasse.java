4d86d9d5cd646cd8198822b94198bf8af8a0e61f
weiss54 <72012859+weiss54@users.noreply.github.com>
2021-12-21 22:57:58 +0100

    Creation de la classe ObjectClasse

---
+++ src/modele/classe/ObjectClasse.java
@@ -0,0 +1,24 @@
+// PACKAGE
+package modele.classe;
+
+/**
+ * Classe ObjectClasse
+ *
+ * Represente une classe java
+ * Contient une liste d'attributs et de methode mais aussi sa position dans la zone d'affichage des diagrammes des classes
+ */
+public abstract class ObjectClasse {
+
+    // ATTRIBUTS
+
+
+    // CONSTRUCTEURS
+
+
+    // METHODES
+
+
+    // GETTERS && SETTERS
+
+
+}
