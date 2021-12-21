f9853f93b52cd00d74620ec38f0612ecbaf11d30
dranerine <74104215+dranerine@users.noreply.github.com>
2021-12-21 15:22:56 +0100

    création de la classe FacadeIntrospection

---
+++ src/introspection/FacadeIntrospection.java
@@ -0,0 +1,29 @@
+package introspection;
+
+import java.io.File;
+
+/**
+ * FacadeIntrospection permet de creer l'ObjetClasse a partir d'un File
+ * 
+ * @author Guillaume renard
+ *
+ */
+public class FacadeIntrospection {
+
+	public FacadeIntrospection() {
+
+	}
+
+	/**
+	 * methode qui permet de construire ObjetClasse a partir du parametre File on
+	 * cherche a avoir la classe objet du File puis on cree ObjetClasse en fonction
+	 * de lui
+	 * 
+	 * @param f
+	 * @return
+	 */
+	public ObjectClasse introspectionClasse(File f) {
+
+	}
+
+}
