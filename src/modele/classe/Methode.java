e28afa13c78206f4e9b20cc9abebbfee691bc30e
weiss54 <72012859+weiss54@users.noreply.github.com>
2021-12-21 22:23:25 +0100

    Creation de la classe Methode

---
+++ src/modele/classe/Methode.java
@@ -0,0 +1,24 @@
+// PACKAGE
+package modele.classe;
+
+/**
+ * Classe Methode
+ *
+ * Represente une methode au sein d'une classe
+ * Contient l'information concernant son nom, son statut, son type de retour et ses parametres
+ */
+public class Methode {
+
+    // ATTRIBUTS
+
+
+    // CONSTRUCTEURS
+
+
+    // METHODES
+
+
+    // GETTERS && SETTERS
+
+
+}
