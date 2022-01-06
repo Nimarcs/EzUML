9fb572b2b20964752b77bc219326fb1049bc6c5e
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-01-06 23:43:42 +0100

    Ajout de la classe Classe

---
+++ src/modele/classe/Classe.java
@@ -0,0 +1,38 @@
+// PACKAGE
+package modele.classe;
+
+/**
+ * Classe Classe
+ *      herite d'Extendable
+ *
+ * Represente un ObjectClasse de type class
+ */
+public class Classe extends Extendable {
+
+    // ATTRIBUTS
+
+    // CONSTRUCTEURS
+
+    /**
+     * Constructeur d'un ObjectClasse, representant une classe java
+     *
+     * @param nomObjectClasse             String: nom de la classe
+     * @param packageObjectClasse         String: nom du package de la classe
+     * @param x                           int: position x de la classe (coin en haut a gauche du rectangle a afficher)
+     * @param y                           int: position y de la classe (coin en haut a gauche du rectangle a afficher)
+     */
+    public Classe(String nomObjectClasse, String packageObjectClasse, int x, int y) {
+        super(nomObjectClasse, packageObjectClasse, x, y);
+    }
+
+    // METHODES
+
+    /**
+     * Getter abstrait sur le type de classe qu'est la classe
+     * @return TypeClasse -> CLASSE
+     */
+    public TypeClasse getType() {
+        return TypeClasse.CLASSE;
+    }
+
+}
