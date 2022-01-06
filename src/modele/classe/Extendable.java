f141dbaec384569882a24166713a60dac371456d
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-01-06 23:44:26 +0100

    Ajout d'attributs, methodes, constructeur et getter dans Extendable

--- src/modele/classe/Extendable.java
+++ src/modele/classe/Extendable.java
@@ -1,17 +1,55 @@
+// PACKAGE
 package modele.classe;
 
-public abstract class Extendable {
+/**
+ * Classe abstraite Extendable
+ *
+ * Represente les ObjectClasse qui herite d'un autre ObjectClasse
+ */
+public abstract class Extendable extends ObjectClasse{
 
     // ATTRIBUTS
-
+    /**
+     * Attribut protegee qui correspond a la classe qui extends cette classe
+     */
+    protected Extendable objectClasseExtends;
 
     // CONSTRUCTEURS
 
+    /**
+     * Constructeur d'un ObjectClasse, representant une classe java
+     *
+     * @param nomObjectClasse     String: nom de la classe
+     * @param packageObjectClasse String: nom du package de la classe
+     * @param x                   int: position x de la classe (coin en haut a gauche du rectangle a afficher)
+     * @param y                   int: position y de la classe (coin en haut a gauche du rectangle a afficher)
+     */
+    public Extendable(String nomObjectClasse, String packageObjectClasse, int x, int y) {
+        super(nomObjectClasse, packageObjectClasse, x, y);
+        this.objectClasseExtends = null;
+    }
 
     // METHODES
 
+    /**
+     * Methode pour changer la classe duquel herite cette classe
+     * si null, cela signifie qu'elle n'herite d'aucune classe
+     *
+     * @param ext Extendable: classe qu'herite cette classe
+     */
+    public void changerExtends(Extendable ext) {
+        this.objectClasseExtends = ext;
+    }
 
     // GETTERS
 
+    /**
+     * Getter de classe qu'hérite cette classe
+     *
+     * @return Extends
+     */
+    public Extendable getObjectClasseExtends() {
+        return objectClasseExtends;
+    }
 
 }
