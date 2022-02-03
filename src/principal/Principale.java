ce4466a4697095f1031a78fb539c236e8f50ea47
RICHIER Marcus <marcus.richier8@etu.univ-lorraine.fr>
2022-02-03 17:04:24 +0100

    Ajout d'un main qui permet de lancer l'interface graphique

---
+++ src/principal/Principale.java
@@ -0,0 +1,64 @@
+package principal;
+
+import modele.Modele;
+import vue.VueArborescence;
+import vue.VueDiagramme;
+
+import javax.swing.*;
+import java.awt.*;
+
+public class Principale {
+
+    /**
+     * Constante de la taille par default du logiciel
+     */
+    private static final int LARGEUR_FEN=750, HAUTEUR_FEN=500, TAILLE_MENU=50, TAILLE_ARBORESCENCE=150;
+
+    /**
+     * Programme principal
+     */
+    public static void main(String[] args) {
+        //Modele
+        Modele modele = new Modele();
+
+        //JFrame
+        JFrame frame = new JFrame("EzUML");
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        //defini la taille minimum de la fenetre
+
+        //ContentPane
+        JPanel contentPane = (JPanel) frame.getContentPane();
+        contentPane.setPreferredSize(new Dimension(LARGEUR_FEN,  HAUTEUR_FEN));
+        contentPane.setLayout(new BorderLayout());
+
+        //Menu
+        JPanel menu = new JPanel();
+        menu.setPreferredSize(new Dimension(LARGEUR_FEN,TAILLE_MENU));
+
+        contentPane.add(menu, BorderLayout.NORTH);
+
+        //PanelPrincipal
+        JSplitPane panelPrincipal = new JSplitPane(JSplitPane.HORIZONTAL_SPLIT);
+        panelPrincipal.setOneTouchExpandable(true);
+
+        contentPane.add(panelPrincipal, BorderLayout.CENTER);
+
+        //VueArborescence
+        VueArborescence vueArborescence = new VueArborescence();
+        panelPrincipal.setDividerLocation(TAILLE_ARBORESCENCE);
+
+        modele.ajouterObservateur(vueArborescence);
+        panelPrincipal.setLeftComponent(vueArborescence);
+
+        //VueDiagramme
+        VueDiagramme vueDiagramme = new VueDiagramme();
+
+        modele.ajouterObservateur(vueDiagramme);
+        panelPrincipal.setRightComponent(vueDiagramme);
+
+        //affichage
+        frame.setVisible(true);
+        frame.pack();
+    }
+
+}
--- src/vue/VueDiagramme.java
+++ src/vue/VueDiagramme.java
@@ -2,7 +2,9 @@ package vue;
 
 import modele.Sujet;
 
-public class VueDiagramme implements Observateur{
+import javax.swing.*;
+
+public class VueDiagramme extends JPanel implements Observateur{ //extends JPanel temporaire
 
 	@Override
 	public void actualiser(Sujet sujet) {
