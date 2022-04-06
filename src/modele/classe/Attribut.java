afab10d63e4fda9158340c53d69038a6919651bc
Nimarcs <moimonautremoi@gmail.com>
2022-04-06 17:31:34 +0200

    Reformatage Attribut

--- src/modele/classe/Attribut.java
+++ src/modele/classe/Attribut.java
@@ -6,7 +6,7 @@ import java.util.regex.Pattern;
 
 /**
  * Classe Attribut
- *
+ * <p>
  * Représente un attribut au sein d'une classe
  * Contient l'information concernant son statut, son type et son nom
  */
@@ -50,11 +50,12 @@ public class Attribut implements Serializable {
 
     /**
      * Constructeur d'un objet Attribut, représentant un attribut au sein d'une classe
-     * @param nomAttribut String: nom de l'attribut
+     *
+     * @param nomAttribut    String: nom de l'attribut
      * @param statutAttribut Statut: statut de l'attribut (PUBLIC, PRIVATE ou PROTECTED)
-     * @param typeAttribut String: type de l'attribut
-     * @param statique boolean: indique si l'attribut est statique ou non
-     * @param finale boolean: indique si l'attribut est final ou non
+     * @param typeAttribut   String: type de l'attribut
+     * @param statique       boolean: indique si l'attribut est statique ou non
+     * @param finale         boolean: indique si l'attribut est final ou non
      */
     public Attribut(String nomAttribut, Statut statutAttribut, String typeAttribut, boolean statique, boolean finale) {
         this.nomAttribut = nomAttribut;
@@ -70,6 +71,7 @@ public class Attribut implements Serializable {
     /**
      * Methode qui retourne le texte permettant l'affichage de l'attribut au sein de la zone d'affichage de diagrammes de classe
      * L'affiche est composé du symbole representant son statut (+, -, #) puis de son nom et pour finir du type de classe de l'attribut
+     *
      * @return String, composé textuel d'affichage de l'attribut
      */
     public String afficher() {
@@ -82,7 +84,7 @@ public class Attribut implements Serializable {
         /*
         Cas où c'est une collection
          */
-        if (this.typeAttribut.contains("<")&&this.typeAttribut.contains(">")) {
+        if (this.typeAttribut.contains("<") && this.typeAttribut.contains(">")) {
             // tabTmp coupe la string en deux a partir du char <
             String[] tabTmp = this.typeAttribut.split(Pattern.quote("<"));
             // dans la partie de gauche de tabTmp (séparé par <) on fait comme pour tabChemin,
@@ -98,27 +100,27 @@ public class Attribut implements Serializable {
                 // on ajoute le symbole coupé
                 type = new StringBuilder(tabChemin2[tabChemin2.length - 1] + "<");
 
-                for (int i=0; i<tabTypes.length; i++) {
+                for (int i = 0; i < tabTypes.length; i++) {
                     // pour chaque string coupé à la virgule on recupere seulement la partie tout a droite apres le dernier point
                     String[] tabLigne = tabTypes[i].split(Pattern.quote("."));
                     type.append(tabLigne[tabLigne.length - 1]);
-                    if (i!=tabTypes.length-1) type.append(",");
+                    if (i != tabTypes.length - 1) type.append(",");
                 }
 
 
-            // cas pour le reste avec 1 seul type
+                // cas pour le reste avec 1 seul type
             } else {
                 String[] tabContenuTemp = tabTmp[1].split(Pattern.quote("."));
                 type = new StringBuilder(tabChemin2[tabChemin2.length - 1] + "<" + tabContenuTemp[tabContenuTemp.length - 1]);
             }
 
-        //Cas pour les tableaux
-        } else if(this.typeAttribut.matches("\\[L(.*)")) {
+            //Cas pour les tableaux
+        } else if (this.typeAttribut.matches("\\[L(.*)")) {
             String[] tabContenu = this.typeAttribut.split(Pattern.quote("."));
-            String val = tabContenu[tabContenu.length-1];
+            String val = tabContenu[tabContenu.length - 1];
             type = new StringBuilder(val.substring(0, val.length() - 1) + " []");
 
-        // sinon, pour un cas simple, on recupere juste le nom du type de l'attribut en fin de string
+            // sinon, pour un cas simple, on recupere juste le nom du type de l'attribut en fin de string
         } else {
             String[] tabChemin = this.typeAttribut.split(Pattern.quote(".")); // tabChemin devise la string par le point
             type = new StringBuilder(tabChemin[tabChemin.length - 1]);
@@ -127,11 +129,12 @@ public class Attribut implements Serializable {
         res += this.nomAttribut + ": " + type;
         return res;
     }
-    
+
     // GETTERS && SETTERS
 
     /**
      * Getter sur le nom de l'attribut
+     *
      * @return String
      */
     public String getNomAttribut() {
@@ -140,6 +143,7 @@ public class Attribut implements Serializable {
 
     /**
      * Methode setter qui permet de changer la visibilite de l'attribut
+     *
      * @param visibilite booleen: parametre indiquant la nouvelle visibilite de l'attribut
      */
     public void changerVisibilite(boolean visibilite) {
@@ -148,6 +152,7 @@ public class Attribut implements Serializable {
 
     /**
      * Getter sur le statut de l'attribut
+     *
      * @return Statut
      */
     public Statut getStatutAttribut() {
@@ -156,6 +161,7 @@ public class Attribut implements Serializable {
 
     /**
      * Getter sur le type de l'attribut
+     *
      * @return String
      */
     public String getTypeAttribut() {
@@ -164,6 +170,7 @@ public class Attribut implements Serializable {
 
     /**
      * Getter sur l'etat statique ou non de l'attribut
+     *
      * @return booleen
      */
     public boolean isStatique() {
@@ -172,6 +179,7 @@ public class Attribut implements Serializable {
 
     /**
      * Getter sur l'etat final ou non de l'attribut
+     *
      * @return booleen
      */
     public boolean isFinale() {
@@ -180,6 +188,7 @@ public class Attribut implements Serializable {
 
     /**
      * Getter sur la visibilite de l'attribut au sein de la zone d'affichage de diagrammes de classe
+     *
      * @return booleen
      */
     public boolean isVisible() {
