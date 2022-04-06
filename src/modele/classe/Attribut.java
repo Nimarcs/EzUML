370552199844230259d3682de0fe4a84d0f15181
Nimarcs <moimonautremoi@gmail.com>
2022-04-06 17:24:36 +0200

    Javadoc attribut

--- src/modele/classe/Attribut.java
+++ src/modele/classe/Attribut.java
@@ -21,11 +21,13 @@ public class Attribut implements Serializable {
 
     /**
      * Attribut prive Statut qui contient le statut de l'attribut
+     * Peut etre privee, public ....
      */
     private Statut statutAttribut;
 
     /**
      * Attribut prive String contenant le type d'objet qu'est l'attribut
+     * Exemple: java.util.List
      */
     private String typeAttribut;
 
@@ -75,48 +77,57 @@ public class Attribut implements Serializable {
         if (this.statutAttribut == Statut.PUBLIC) res += "+ ";
         else if (this.statutAttribut == Statut.PRIVATE) res += "- ";
         else res += "# ";
-        String type;
+        StringBuilder type;
 
         /*
         Cas où c'est une collection
          */
         if (this.typeAttribut.contains("<")&&this.typeAttribut.contains(">")) {
-            String tabTmp[] = this.typeAttribut.split(Pattern.quote("<")); // tabTmp coupe la string en deux a partie du char <
-            String tabChemin2[] = tabTmp[0].split(Pattern.quote(".")); // dans la partie de gauche de tabTmp (séparé par <) on fait comme pour tabChemin, on recupere la string la plus a droite séparé par un point
-            if (this.typeAttribut.contains(",")) { // cas pour les collections avec 2 type et plus
-                String tabTypes[] = tabTmp[1].split(Pattern.quote(",")); // on prend la partie droite de tabTmp et on coupe a partir des virgules
-                type = tabChemin2[tabChemin2.length-1] + "<"; // on ajoute le symbole coupé
+            // tabTmp coupe la string en deux a partir du char <
+            String[] tabTmp = this.typeAttribut.split(Pattern.quote("<"));
+            // dans la partie de gauche de tabTmp (séparé par <) on fait comme pour tabChemin,
+            // on recupere la string la plus a droite séparé par un point
+            String[] tabChemin2 = tabTmp[0].split(Pattern.quote("."));
+
+
+            // cas pour les collections avec 2 type et plus
+            if (this.typeAttribut.contains(",")) {
+                // on prend la partie droite de tabTmp et on coupe a partir des virgules
+                String[] tabTypes = tabTmp[1].split(Pattern.quote(","));
+
+                // on ajoute le symbole coupé
+                type = new StringBuilder(tabChemin2[tabChemin2.length - 1] + "<");
+
                 for (int i=0; i<tabTypes.length; i++) {
-                    String tabLigne[] = tabTypes[i].split(Pattern.quote(".")); // pour chaque string coupé à la virgule on recupere seulement la partie tout a droite apres le dernier point
-                    type+=tabLigne[tabLigne.length-1];
-                    if (i!=tabTypes.length-1) type+=",";
+                    // pour chaque string coupé à la virgule on recupere seulement la partie tout a droite apres le dernier point
+                    String[] tabLigne = tabTypes[i].split(Pattern.quote("."));
+                    type.append(tabLigne[tabLigne.length - 1]);
+                    if (i!=tabTypes.length-1) type.append(",");
                 }
 
-            } else { // cas pour le reste avec 1 seul type
-                String tabContenuTemp[] = tabTmp[1].split(Pattern.quote("."));
-                type = tabChemin2[tabChemin2.length - 1] + "<" + tabContenuTemp[tabContenuTemp.length - 1];
+
+            // cas pour le reste avec 1 seul type
+            } else {
+                String[] tabContenuTemp = tabTmp[1].split(Pattern.quote("."));
+                type = new StringBuilder(tabChemin2[tabChemin2.length - 1] + "<" + tabContenuTemp[tabContenuTemp.length - 1]);
             }
+
+        //Cas pour les tableaux
         } else if(this.typeAttribut.matches("\\[L(.*)")) {
-            String tabContenu[] = this.typeAttribut.split(Pattern.quote("."));
+            String[] tabContenu = this.typeAttribut.split(Pattern.quote("."));
             String val = tabContenu[tabContenu.length-1];
-            type = val.substring(0, val.length()-1)+" []";
-        } else { // sinon, pour un cas simple, on recupére juste le nom du type de l'attribut en fin de string
+            type = new StringBuilder(val.substring(0, val.length() - 1) + " []");
+
+        // sinon, pour un cas simple, on recupere juste le nom du type de l'attribut en fin de string
+        } else {
             String[] tabChemin = this.typeAttribut.split(Pattern.quote(".")); // tabChemin devise la string par le point
-            type = tabChemin[tabChemin.length-1];
+            type = new StringBuilder(tabChemin[tabChemin.length - 1]);
         }
 
         res += this.nomAttribut + ": " + type;
         return res;
     }
-
-    /**
-     * Methode setter qui permet de changer la visibilite de l'attribut
-     * @param visibilite booleen: parametre indiquant la nouvelle visibilite de l'attribut
-     */
-    public void changerVisibilite(boolean visibilite) {
-        this.visible = visibilite;
-    }
-
+    
     // GETTERS && SETTERS
 
     /**
@@ -125,6 +136,14 @@ public class Attribut implements Serializable {
      */
     public String getNomAttribut() {
         return this.nomAttribut;
+    }
+
+    /**
+     * Methode setter qui permet de changer la visibilite de l'attribut
+     * @param visibilite booleen: parametre indiquant la nouvelle visibilite de l'attribut
+     */
+    public void changerVisibilite(boolean visibilite) {
+        this.visible = visibilite;
     }
 
     /**
