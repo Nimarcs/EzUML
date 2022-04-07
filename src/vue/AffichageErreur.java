1a5cecb4bf68743d029c701f25be68a226b65165
weiss54 <72012859+weiss54@users.noreply.github.com>
2022-04-07 10:35:03 +0200

    Javadoc AffichageErreur

--- src/vue/AffichageErreur.java
+++ src/vue/AffichageErreur.java
@@ -4,7 +4,7 @@ import javax.swing.*;
 
 /**
  * Classe qui va permettre l'affichage d'erreur sous la forme de popup
- *
+ * <p>
  * Singleton adapte pour etre sur d'avoir acces a la frame de l'application
  */
 public class AffichageErreur {
@@ -23,37 +23,43 @@ public class AffichageErreur {
     /**
      * Constructeur de AffichageErreur
      * Appelee par la methode instancier
+     *
      * @param oldF Frame de l'application principale
      */
-    private AffichageErreur(JFrame oldF){
+    private AffichageErreur(JFrame oldF) {
         oldFrame = oldF;
     }
 
     /**
      * Methode a appeler au debut du programme
      * permet d'instancier l'affichage d'erreur qui pourra etre appelee depuis n'importe ou ensuite
+     *
      * @param jFrame Frame du programme principal
      */
     public synchronized static void instancier(JFrame jFrame) {
         if (affichageErreur != null) throw new IllegalStateException("Affichage erreur deja instancie");
-        if (jFrame == null) throw new NullPointerException("La JFrame doit etre non null et correspondre a votre fenetre principale");
+        if (jFrame == null)
+            throw new NullPointerException("La JFrame doit etre non null et correspondre a votre fenetre principale");
         affichageErreur = new AffichageErreur(jFrame);
     }
 
     /**
      * Methode qui permet de recuperer l'instance
+     *
      * @return Instance de AffichageErreur
      */
     public synchronized static AffichageErreur getInstance() {
-        if (affichageErreur == null) throw new IllegalStateException("Vous devez instancier l'affichage de l'erreur prealablement");
+        if (affichageErreur == null)
+            throw new IllegalStateException("Vous devez instancier l'affichage de l'erreur prealablement");
         return affichageErreur;
     }
 
     /**
      * Methode qui permet d'afficher un message d'erreur
+     *
      * @param message message d'erreur a afficher
      */
-    public void afficherErreur(String message){
+    public void afficherErreur(String message) {
         JOptionPane.showMessageDialog(oldFrame, message, "Erreur", JOptionPane.ERROR_MESSAGE);
         oldFrame.requestFocus();
     }
@@ -61,9 +67,10 @@ public class AffichageErreur {
 
     /**
      * Methode qui permet d'afficher un message
+     *
      * @param message message d'erreur a afficher
      */
-    public void afficherMessage(String message){
+    public void afficherMessage(String message) {
         JOptionPane.showMessageDialog(oldFrame, message, "Message Systeme", JOptionPane.INFORMATION_MESSAGE);
         oldFrame.requestFocus();
     }
